2136e06f1f5542445ec8ab7061afd832b22d0d21
Edgarcia09 <edgar.garcia@uip.edu.pa>
2025-10-24 15:54:12 -0500

    agregue funciones obtener_fecha_actual() y pausar_programa() al modulo utilidades.

--- Proyecto-M3-/modulos/utilidades.py
+++ Proyecto-M3-/modulos/utilidades.py
@@ -1,8 +1,26 @@
-def saludar ():
-    print ("Hola! Buenos dias!")
+from datetime import datetime
 
-def despedida ():
-    print ("Nos vemos mañana!")
+
+def saludar():
+    print("Hola! Buenos dias!")
+
+
+def despedida():
+    print("Nos vemos mañana!")
+
 
 saludar()
 despedida()
+
+# ==========================
+# Funciones añadidas por Edgarcia09
+# ==========================
+
+
+def obtener_fecha_actual():
+    fecha_actual = datetime.now()
+    return fecha_actual.strftime("%d/%m/%Y")
+
+
+def pausar_programa():
+    input("Presiona Enter para continuar...")
