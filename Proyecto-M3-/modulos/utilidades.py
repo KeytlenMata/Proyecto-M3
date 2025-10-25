ca8822d9718763861b65f18b046f7670025f2dd6
Keytlen <keytlen.mata@uip.edu.pa>
2025-10-25 00:27:29 -0500

    Reorganización de funciones en utilidades.py: fecha y pausa antes de despedida

--- Proyecto-M3-/modulos/utilidades.py
+++ Proyecto-M3-/modulos/utilidades.py
@@ -1,22 +1,14 @@
 from datetime import datetime
 
-
 def saludar():
     print("Hola! Buenos dias!")
-
-
-def despedida():
-    print("Nos vemos mañana!")
-
-# ==========================
-# Funciones añadidas por Edgarcia09
-# ==========================
-
 
 def obtener_fecha_actual():
     fecha_actual = datetime.now()
     return fecha_actual.strftime("%d/%m/%Y")
 
-
 def pausar_programa():
     input("Presiona Enter para continuar...")
+
+def despedida():
+    print("Nos vemos mañana!")
